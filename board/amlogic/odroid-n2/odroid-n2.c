1bd61c24f169f74e0d5c1b7051a9a8b5ad518034
Stanislav Vlasic <svlasic@gmail.com>
2021-05-25 18:20:22 +0200

    board: amlogic: odroid-n2: generate serial# on boot

--- board/amlogic/odroid-n2/odroid-n2.c
+++ board/amlogic/odroid-n2/odroid-n2.c
@@ -22,6 +22,11 @@ int misc_init_r(void)
 	u8 mac_addr[MAC_ADDR_LEN];
 	char efuse_mac_addr[EFUSE_MAC_SIZE], tmp[3];
 	ssize_t len;
+	char chip_serial[16];
+	char serial_string[12];
+	u8 serial[6];
+	u32 sid;
+	u16 sid16;
 
 	meson_eth_init(PHY_INTERFACE_MODE_RGMII, 0);
 
@@ -43,6 +48,29 @@ int misc_init_r(void)
 			eth_env_set_enetaddr("ethaddr", mac_addr);
 		else
 			meson_generate_serial_ethaddr();
+	}
+
+	if (!env_get("serial#")) {
+		if (!meson_sm_get_serial(chip_serial, SM_SERIAL_SIZE)) {
+			sid = crc32(0, (unsigned char *)chip_serial, SM_SERIAL_SIZE);
+			sid16 = crc16_ccitt(0, (unsigned char *)chip_serial,	SM_SERIAL_SIZE);
+
+			/* Ensure the NIC specific bytes of the mac are not all 0 */
+			if ((sid & 0xffffff) == 0)
+				sid |= 0x800000;
+
+			/* Non OUI  */
+			serial[0] = ((sid16 >> 8) & 0xfc) | 0x02;
+			serial[1] = (sid16 >>  0) & 0xff;
+			serial[2] = (sid >> 24) & 0xff;
+			serial[3] = (sid >> 16) & 0xff;
+			serial[4] = (sid >>  8) & 0xff;
+			serial[5] = (sid >>  0) & 0xff;
+			sprintf(serial_string, "%02X%02X%02X%02X%02X%02X", serial[0], serial[1], serial[2],serial[3], serial[4], serial[5]);
+			env_set("serial#", serial_string);
+
+		} else
+			return -EINVAL;
 	}
 
 	return 0;
